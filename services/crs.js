1c61d6c58956d382be1699c12730f1bf29086a5a
Trzaska <NiklasTrzaska@uni-muenster.de>
2016-10-31 09:13:12 +0100

    Improving error-message.

--- services/crs.js
+++ services/crs.js
@@ -1,5 +1,6 @@
 function get() {
-  var supportedCRS = [[
+  var supportedCRS = [
+    [
       //WGS48
       'EPSG:4326',
       '+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees'
@@ -25,6 +26,11 @@ function get() {
       '+proj=cass +lat_0=52.41864827777778 +lon_0=13.62720366666667 +x_0=40000 +y_0=10000 +ellps=bessel +towgs84=598.1,73.7,418.2,0.202,0.045,-2.455,6.7 +units=m +no_defs'
   ],
 
+  [
+      //Pseudo-Mercator / Web-Mercator
+      'EPSG:3857',
+      '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs'
+  ]
 
   ];
 
