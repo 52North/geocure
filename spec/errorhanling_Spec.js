d99e63b0167137afc3fa653d5b5203090b6de4ce
Niklas Trzaska <niklas.trzaska@uni-muenster.de>
2016-11-22 17:59:57 +0100

    Initial setup.

---
+++ spec/errorhanling_Spec.js
@@ -0,0 +1,29 @@
+var errorHandling = require("../app/general/errorhandling.js")
+
+describe("The function app/general/errorhandling.getError", function() {
+
+
+    it("maps from app/services/statuscodeMapping.json without additional message", function(done) {
+        let errormessage = errorHandling.getError("services", "schemaError")
+        let testobj = {
+            "code" : 1100,
+            "description" : "The file 'app/services/services.json' does not correspond to 'app/services/servicesMetadescription.json'.",
+        }
+
+        expect(errormessage).toEqual(testobj);
+        done();
+    });
+
+    it("maps from app/services/statuscodeMapping.json with additional message", function(done) {
+        let errormessage = errorHandling.getError("services", "schemaError", "Things went wrong")
+        let testobj = {
+            "code" : 1100,
+            "description" : "The file 'app/services/services.json' does not correspond to 'app/services/servicesMetadescription.json'.",
+            "info" : "Things went wrong",
+        }
+
+
+        expect(errormessage).toEqual(testobj);
+        done();
+    });
+});
