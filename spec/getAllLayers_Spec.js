7db928831a9268dc28eeb6f8bf0d556773e7fb16
Niklas Trzaska <niklas.trzaska@uni-muenster.de>
2016-12-01 11:47:29 +0100

    Change false to true due to unintended saves after tests.

--- spec/getAllLayers_Spec.js
+++ spec/getAllLayers_Spec.js
@@ -3,7 +3,7 @@ const dummyGetCapabilitirs = require("./expetedWMSGetCapabilities.js");
 
 describe("getAllLayers", () => {
   it("returns the expected Collection for the dummyGetCapabilitirs", done => {
-    const response = maps.getAllLayers(dummyGetCapabilitirs.get(), {"fullUrl" : "DummyFullURL"});
+    const response = maps.getAllLayers({"capabilities": dummyGetCapabilitirs.get()}, {"fullUrl" : "DummyFullURL"});
     const expected = [{"id":"areas","title":"areas","href":"DummyFullURL/render"},{"id":"points","title":"points","href":"DummyFullURL/render"}];
     expect(response).toEqual(expected);
     done();
