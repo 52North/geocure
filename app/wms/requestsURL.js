dd8c8760150661d87a29f889b3096b52caf54f65
Niklas Trzaska <niklas.trzaska@uni-muenster.de>
2016-12-04 20:02:08 +0100

    Implementing default Bbox.

--- app/wms/requestsURL.js
+++ app/wms/requestsURL.js
@@ -1,4 +1,5 @@
 const generalURLConstructor = require("../general/url.js");
+const coordinates = require("../general/coordinates.js")
 const errorhandling = require("../general/errorhandling.js");
 const maps = require("./maps.js");
 const transformationParameters = require("../config/transformationParameter.js");
@@ -6,15 +7,15 @@ const transformationParameters = require("../config/transformationParameter.js")
 // Constants
 const version = "1.3.0";
 const defaultCRS = "EPSG:4326"
-/**
- * Takes the URL of an service and creates the basic part for all requests.
- * Basic part consists of: the requested service ("wms") and the version.
- * @method getCapabilities
- * @param  {String}        serviceURL The Baseurl to the service
- * @return {String}                   The constructed url
- */
+        /**
+         * Takes the URL of an service and creates the basic part for all requests.
+         * Basic part consists of: the requested service ("wms") and the version.
+         * @method getCapabilities
+         * @param  {String}        serviceURL The Baseurl to the service
+         * @return {String}                   The constructed url
+         */
 function getCapabilities(serviceURL) {
-  "use strict"
+        "use strict"
 
         let url = generalURLConstructor.getBaseURL(serviceURL, ["wms", version]);
         return url + "&REQUEST=getCapabilities";
@@ -23,7 +24,7 @@ function getCapabilities(serviceURL) {
 
 
 function getMap(serviceURL, requestargs) {
-  "use strict"
+        "use strict"
 
         let baseURL = generalURLConstructor.getBaseURL(serviceURL, ["wms", version]) + "&REQUEST=GetMap";
 
@@ -33,30 +34,90 @@ function getMap(serviceURL, requestargs) {
 
 
 
-function* GetMapURLGenerator(serviceCache, baseURL, requestargs) {
-  "use strict"
+function GetMapURL(serviceCache, baseURL, requestargs) {
+        "use strict"
 
         let url = baseURL;
 
         // Adding Layers
-        try{
-          layerValid(serviceCache, requestargs);
-          url += "&LAYERS=" + requestargs.layers;
-        }
-        catch (error) {
-          throw error
+        try {
+                layerValid(serviceCache, requestargs);
+                url += "&LAYERS=" + requestargs.layers;
+        } catch (error) {
+                throw error
         }
 
         // As no seperated styling is supported. So styles is empty to use default styling
         url += "&STYLES=";
 
         // Adding CRS
-        try{
-          const crs = getCRS(serviceCache, requestargs);
+        try {
+                const crs = getCRS(serviceCache, requestargs);
+        } catch (error) {
+          throw error;
         }
-        catch (error) {
 
-        }
+        url += "&CRS=" + crs;
+
+
+        // Adding bbox
+
+
+}
+
+
+// TODO bbox realisieren.
+// function getBbox(capabilities, requestargs) {
+// "use strict";
+//   // If no bbox is given, return a default-bbox.
+//   // If no crs is given, use EPGS:4326
+//   // Otherwise use cooedinates in the given crs.
+//   // This is important, because images can be returned with a spatial reference.
+//   if(!requestargs.bbox){
+//     const defaultBbox(capabilities, requestargs);
+//   }
+//   else{
+//
+//   }
+// }
+
+/**
+ * Returns a stringrepresentation of the bbox, which is maximum in extend.
+ * If no crs is given: EPSG:4326
+ * Otherwise the given system if valid
+ * @param  {Object}       capabilities Capabilities of the requested service.
+ * @param  {Object}       requestargs  The arguments from the requestargs
+ * @return {String}                    Concatenation if minx,miny,maxx,maxy
+ * @throws {Error}                     Otherwise
+ */
+function getdefaultBbox(capabilities, requestargs){
+  "use strict";
+  try{
+    const maxBbox = capabilities.capabilities.WMS_Capabilities.capability.layer.exGeographicBoundingBox;
+
+    if(!maxBbox){
+      throw errorhandling.getError("requestResponses", "badCapabilitiesAccess", "Tried to get 'exGeographicBoundingBox'");
+    }
+
+    const targetCrs = getCRS(capabilities, requestargs);
+
+    if(targetCrs === "EPSG:4326"){
+      //then no transformation is needed, because maxBbox is given in EGPS:4326 via specification.
+      return "" + maxBbox.westBoundLongitude + "," + maxBbox.southBoundLatitude + "," + maxBbox.eastBoundLongitude + "," + maxBbox.northBoundLatitude;
+    }
+    else {
+      // Targetsystem validation already happened in getCrs
+      const minis = coordinates.transformation(maxBbox.westBoundLongitude, maxBbox.southBoundLatitude, "EPSG:4326", targetCrs);
+      const maxis = coordinates.transformation(maxBbox.eastBoundLongitude, maxBbox.northBoundLatitude, "EPSG:4326", targetCrs);
+
+      return "" + minis.x + "," + minis.y + "," + maxis.x + "," + maxis.y;
+    }
+
+  }
+  catch (error) {
+    throw error;
+  }
+
 }
 
 
@@ -70,24 +131,25 @@ function* GetMapURLGenerator(serviceCache, baseURL, requestargs) {
  * @return {String}              EPSG Code of a crs
  * @throws {Error}               Otherwise
  */
-function getCRS(capabilities, requestargs){
-  "use strict"
-  if(!requestargs.crs){
-    return defaultCRS;
-  }
-  else {
+function getCRS(capabilities, requestargs) {
+        "use strict"
+        if (!requestargs.crs) {
+                return defaultCRS;
+        } else {
 
-    var supportedByTransformationParameter = transformationParameters.get().find(element => {return element[0] === requestargs.crs})
-    var supportedByGetCapabilities = capabilities.capabilities.WMS_Capabilities.capability.layer.crs.find(element => {return element === requestargs.crs});
+                var supportedByTransformationParameter = transformationParameters.get().find(element => {
+                        return element[0] === requestargs.crs
+                })
+                var supportedByGetCapabilities = capabilities.capabilities.WMS_Capabilities.capability.layer.crs.find(element => {
+                        return element === requestargs.crs
+                });
 
-    if (supportedByTransformationParameter && supportedByGetCapabilities) {
-      return requestargs.crs;
-    }
-    else {
-      throw errorhandling.getError("requestResponses", "crs", requestargs.crs);
-    }
-
-  }
+                if (supportedByTransformationParameter && supportedByGetCapabilities) {
+                        return requestargs.crs;
+                } else {
+                        throw errorhandling.getError("requestResponses", "crs", requestargs.crs);
+                }
+        }
 }
 
 
@@ -99,7 +161,7 @@ function getCRS(capabilities, requestargs){
  * @throws {Exception}             Esle
  */
 function layerValid(capabilities, requestargs) {
-  "use strict"
+        "use strict"
 
         // We need at least one layer for a request
         if (requestargs.layers === "undefined") {
@@ -135,5 +197,6 @@ module.exports = {
         getCapabilities: getCapabilities,
         getMap: getMap,
         layerValid: layerValid,
-        getCRS: getCRS
+        getCRS: getCRS,
+        getdefaultBbox: getdefaultBbox
 }
