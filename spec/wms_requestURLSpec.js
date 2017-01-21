55f02806e501b6c483b93d103e951fada1b2a464
Niklas Trzaska <niklas.trzaska@uni-muenster.de>
2017-01-21 19:29:35 +0100

    spec update

--- spec/wms_requestURLSpec.js
+++
@@ -1,9 +0,0 @@
-const requestURL = require("../app/wms/requestsURL.js");
-
-describe("getCapabilities", function(){
-  it("returns the expeded concatenation", done => {
-    const res = requestURL.getCapabilities("abc");
-    expect(res).toBe("abc/wms?SERVICE=wms&VERSION=1.3.0&REQUEST=getCapabilities")
-    done();
-  })
-});
